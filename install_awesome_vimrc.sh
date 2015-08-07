41dfdd624197f2fe1e3bc946a14f1566ecde7353
Forilan <1096777077@qq.com>
2015-08-07 16:09:11 +0800

    Update install_awesome_vimrc.sh

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -1,14 +1,14 @@
-cd ~/.vim_runtime
+cd ~/.vim_forilan
 
-echo 'set runtimepath+=~/.vim_runtime
+echo 'set runtimepath+=~/.vim_forilan
 
-source ~/.vim_runtime/vimrcs/basic.vim
-source ~/.vim_runtime/vimrcs/filetypes.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-source ~/.vim_runtime/vimrcs/extended.vim
+source ~/.vim_forilan/vimrcs/basic.vim
+source ~/.vim_forilan/vimrcs/filetypes.vim
+source ~/.vim_forilan/vimrcs/plugins_config.vim
+source ~/.vim_forilan/vimrcs/extended.vim
 
 try
-source ~/.vim_runtime/my_configs.vim
+source ~/.vim_forilan/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
